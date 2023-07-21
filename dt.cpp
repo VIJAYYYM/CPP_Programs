cc3eb12a4dfad6f005ee86f1ad4b5fe960d149d7
vijayMu <129942667+vijayMu@users.noreply.github.com>
2023-07-21 06:48:08 +0530

    Add files via upload

---
+++ dt.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int x = 10;
+    long y = 15;
+    std::cin >> x;
+    long long z = 150000000;
+    float a = 5.6;
+    float b = 5;
+    std::cout << "Value of y:" << b;
+    return 0;
+}
+
